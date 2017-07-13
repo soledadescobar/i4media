1269a1639e7fb9e38a4a5241176272bafad5e29f
scarcellaga <scarcella.ga@gmail.com>
2017-07-13 19:25:07 -0300

    Improves on ShellApi. Addedcommand line args for search.py

--- shell/search.py
+++ shell/search.py
@@ -8,17 +8,14 @@ Proccess API Search from Keywords stored in keywords.json.
 """)
 parser.add_argument(
     ['-k', '--keywords'],
-    dest='keywords',
     help="Look this keywords object in keywords.json. Default=keywords",
     default='keywords')
 parser.add_argument(
     ['-u', '--until'],
-    dest='until',
     help="Define the max date to search. Default None",
     default=None)
 parser.add_argument(
     ['-l', '--limit'],
-    dest='limit',
     help="Limit of returned tweets / 100. Default 10, will return 1000 tweets",
     default=10)
 args = parser.parse_args()

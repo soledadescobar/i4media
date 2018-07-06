dc7a804337156ef3e54e545563a44631bdc7bd4a
Gabriel Scarcella <scarcella.ga@gmail.com>
2018-07-06 12:32:44 -0300

    Mejorando Scripts SH

--- install.sh
+++ install.sh
@@ -1,8 +1,62 @@
-apt update
-apt install -y python-pip python-systemd
-pip install -r requirements.txt
-cp -f i4media-twitter.sh /etc/init.d/i4media-twitter.sh
-chmod +755 /etc/init.d/i4media-twitter.sh
-systemctl daemon-reload
-service i4media-twitter start
+#!/usr/bin/env bash
+GREEN=`tput setaf 2`
+RED=`tput setaf 1`
+BLUE=`tput setaf 3`
+RESET=`tput sgr0`
+
+GIT_USERNAME="gascarcella"
+GIT_PASSWORD="gorila38"
+
+GIT_URL="https://$GIT_USERNAME:$GIT_PASSWORD@github.com/gascarcella/i4media.git"
+
+DEFAULT_FOLDER=/usr/lib/i4media
+
+command -v git >/dev/null 2>&1 || {
+    echo "${RED}GIT Not detected.${BLUE}Installing${RESET}"
+    sudo apt update
+    sudo apt install -y git
+}
+
+command -v python3 >/dev/null 2>&1 || {
+    echo "Python3 Not detected. Installing"
+    sudo apt update
+    sudo apt install -y python3
+}
+command -v pip3 >/dev/null 2>&1 || {
+    echo "Pip3 Not detected. Installing"
+    sudo apt update
+    sudo apt install -y python3-pip
+}
+echo "${BLUE}Trying to install python3-systemd tools${RESET}"
+sudo apt install python3-systemd
+
+echo "${GREEN}====================================================${RESET}"
+echo " "
+echo "${BLUE}Specify installation folder. Leave blank for default [$DEFAULT_FOLDER]${RESET}"
+
+read -p "${BLUE}Absolute Path: ${GREEN}[$DEFAULT_FOLDER]${RESET}" OUTPUT_FOLDER
+
+:${OUTPUT_FOLDER:=${DEFAULT_FOLDER}}
+
+echo "${BLUE}Creating Output Folder $OUTPUT_FOLDER${RESET}"
+sudo mkdir -p ${OUTPUT_FOLDER}
+sudo chown ${USER}:${USER} ${OUTPUT_FOLDER}
+
+echo "${BLUE}Entering $OUTPUT_FOLDER${RESET}"
+cd ${OUTPUT_FOLDER}
+
+git clone ${GIT_URL}
+cd i4media
+
+echo "${BLUE}Installing PIP Requirements${RESET}"
+sudo -H pip3 install -r requirements.txt
+
+chmod +x install-stream.sh
+./install-stream.sh
+
+if [ "$OUTPUT_FOLDER" != "$DEFAULT_FOLDER" ]; then
+    echo "${RED}You are not using the default folder. Remember to edit /etc/init.d/i4media.twitter-stream.sh with the fixed path${RESET}"
+fi
+
+echo "${GREEN}Finish${RESET}"
 exit
--- twitter-stream.sh
+++ twitter-stream.sh
@@ -1,33 +1,33 @@
 #!/bin/sh
 
 ### BEGIN INIT INFO
-# Provides:          i4media-stream
+# Provides:          i4media.twitter-stream
 # Required-Start:    $remote_fs $syslog
 # Required-Stop:     $remote_fs $syslog
 # Default-Start:     2 3 4 5
 # Default-Stop:      0 1 6
-# Short-Description: i4media streaming tool
+# Short-Description: i4media twitter streaming service
 # Description:       Services Provided: Streaming, Updater, Bridge (5501)
 ### END INIT INFO
 
-DIR=/home/gabriel/pst/i4media
-DAEMON=$DIR/control.py
-DAEMON_NAME=i4media-stream
+DIR=/usr/lib/i4media/i4media
+DAEMON=${DIR}/control.py
+DAEMON_NAME=i4media.twitter-stream
 DAEMON_OPTS="--stream"
 DAEMON_USER=root
 
-PIDFILE=/var/run/$DAEMON_NAME.pid
+PID_FILE=/var/run/${DAEMON_NAME}.pid
 
 . /lib/lsb/init-functions
 
 do_start () {
     log_daemon_msg "Starting $DAEMON_NAME with options $DAEMON_OPTS"
-    start-stop-daemon --start --background --oknodo --pidfile $PIDFILE --make-pidfile --user $DAEMON_USER --chuid $DAEMON_USER --startas $DAEMON -- $DAEMON_OPTS
+    start-stop-daemon --start --background --oknodo --pidfile ${PID_FILE} --make-pidfile --user ${DAEMON_USER} --chuid ${DAEMON_USER} --startas ${DAEMON} -- ${DAEMON_OPTS}
     log_end_msg $?
 }
 do_stop () {
     log_daemon_msg "Stopping system $DAEMON_NAME daemon"
-    start-stop-daemon --stop --pidfile $PIDFILE --retry 10
+    start-stop-daemon --stop --pidfile ${PID_FILE} --retry 10
     log_end_msg $?
 }
 
